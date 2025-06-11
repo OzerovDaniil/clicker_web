7a29ce68cd44dfc9384284654f2212ea204371a3
Daniil <danil20052309@gmail.com>
2025-06-11 14:34:21 +0300

    Refactor Vite configuration by removing unused options and cleaning up code structure

--- music-magnate/vite.config.js
+++ music-magnate/vite.config.js
@@ -1,33 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/clicker_web/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly'
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
-  }
-}); 
+});
